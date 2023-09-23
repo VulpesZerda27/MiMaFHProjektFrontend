844008d6e1ca69f339c2e9ad590bf93d077a45f4
martin <martin@dankeschatz.com>
2023-09-23 14:27:55 +0200

    changed config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 //window.BACKEND_URL = "http://localhost:8080";
-window.BACKEND_URL = "http://backend:8080"
+window.BACKEND_URL = "https://api.bookshop.vulpeszerda.at";
 window.ADMIN_ENDPOINT = window.BACKEND_URL + "/admin";
 window.USER_ENDPOINT = window.BACKEND_URL + "/user";
 window.ADMIN_USER_ENDPOINT = window.BACKEND_URL + window.ADMIN_ENDPOINT + window.USER_ENDPOINT;
